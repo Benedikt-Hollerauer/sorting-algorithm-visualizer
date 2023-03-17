593ae7c28a9712b8e24b5f1b409251a38b86f63a
Benedikt Hollerauer <hollibene@gmail.com>
2023-03-17 07:21:42 +0100

    refactored sortDescendingWithIntermediateResults to be simpler for further implementation

--- src/core/entity/BubbleSortEntity.scala
+++ src/core/entity/BubbleSortEntity.scala
@@ -14,12 +14,9 @@ object BubbleSortEntity:
 			).map(x => SortableValue.from(x).toOption.get)
 
 	def sortDescendingWithIntermediateResults(toBeSorted: SortableValue): LazyList[SortableValue] =
-		val res = LazyList.from(toBeSorted.list)
-			.zipWithIndex
-			.scanLeft(toBeSorted.list)((acc, s) =>
-				if(!acc.isDefinedAt(s._2 + 1)) acc
-				else if(acc(s._2 + 1) >= s._1) acc
-				else acc.updated(s._2, acc(s._2 + 1)).updated(s._2 + 1, acc(s._2))
-			).map(x => SortableValue.from(x).toOption.get)
-		res.foreach(println)
-		res
+		LazyList.from(toBeSorted.list)
+			.scanLeft(toBeSorted.list)((acc, it) =>
+				acc
+			).foreach(println)
+
+		LazyList(SortableValue.from(List(1, 2, 3)).toOption.get)
