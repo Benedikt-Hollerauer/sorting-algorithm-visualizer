66ffc30daf58ca07a65f3fb220c8386990df33ce
Benedikt Hollerauer <88812164+Benedikt-Hollerauer@users.noreply.github.com>
2023-03-21 14:05:22 +0100

    implemented first iteration of bubble sort

--- src/core/entity/BubbleSortEntity.scala
+++ src/core/entity/BubbleSortEntity.scala
@@ -6,12 +6,13 @@ object BubbleSortEntity:
 
 	def sortAscendingWithIntermediateResults(toBeSorted: SortableValue): LazyList[SortableValue] =
 		LazyList.from(toBeSorted.list)
-			.zipWithIndex
-			.scanLeft(toBeSorted.list)((acc, s) =>
-				if(!acc.isDefinedAt(s._2 + 1)) acc
-				else if(acc(s._2 + 1) >= s._1) acc
-				else acc.updated(s._2, acc(s._2 + 1)).updated(s._2 + 1, acc(s._2))
-			).map(x => SortableValue.from(x).toOption.get)
+			.scanLeft(List.empty[Int])((acc, s) =>
+				acc.lastOption match
+					case Some(f) =>
+						if (f > s) (acc.dropRight(1) :+ s) :+ f
+						else acc :+ s
+					case None => acc :+ s
+			).map(list => list :: toBeSorted.list.map(x => List(x)).drop(list.length)).map(_.flatten).map(x => SortableValue.from(x).toOption.get)
 
 	def sortDescendingWithIntermediateResults(toBeSorted: SortableValue): LazyList[SortableValue] =
 		LazyList.from(toBeSorted.list)
@@ -21,6 +22,4 @@ object BubbleSortEntity:
 						if(f > s) (acc.dropRight(1) :+ s) :+ f
 						else acc :+ s
 					case None => acc :+ s
-			).foreach(println)
-
-		LazyList(SortableValue.from(List(1, 2, 3)).toOption.get)
+			).map(list => list :: toBeSorted.list.map(x => List(x)).drop(list.length)).map(_.flatten).map(x => SortableValue.from(x).toOption.get)
