7147191e480aabfc2987b344a88f5f29e2da35fa
Benedikt Hollerauer <88812164+Benedikt-Hollerauer@users.noreply.github.com>
2023-03-23 14:44:42 +0100

    implmented SortByBubbleSortUseCase

--- src/core/useCase/SortByBubbleSortUseCase.scala
+++ src/core/useCase/SortByBubbleSortUseCase.scala
@@ -1,9 +1,17 @@
 package useCase
 
+import core.entity.BubbleSortEntity
+import core.value.OrderValue.{Ascending, Descending}
 import core.value.SortableValue
 import error.useCaseError.SortByBubbleSortUseCaseError
 import input.SortByBubbleSortInput
 
 object SortByBubbleSortUseCase:
 
-    def apply(input: SortByBubbleSortInput): Either[SortByBubbleSortUseCaseError, LazyList[SortableValue]] = ???
+    def apply(input: SortByBubbleSortInput): Either[SortByBubbleSortUseCaseError, LazyList[SortableValue]] =
+        for
+            toBeSorted <- SortableValue.from(input.list).left.map(SortByBubbleSortUseCaseError.InputFailure(_))
+            sorted = input.ordering match
+                case Ascending => BubbleSortEntity.sortAscendingWithIntermediateResults(toBeSorted)
+                case Descending => BubbleSortEntity.sortDescendingWithIntermediateResults(toBeSorted)
+        yield sorted
