ff4610185635b4caa60798ac4c8067641c34be47
Benedikt Hollerauer <hollibene@gmail.com>
2023-04-12 07:37:19 +0200

    added GenerateSortableUseCase to Main

--- presentation/Main.scala
+++ presentation/Main.scala
@@ -1,16 +1,25 @@
 import com.raquo.laminar.api.L.{*, given}
+import com.raquo.laminar.nodes.ReactiveElement
 import core.input.SortByBubbleSortInput
 import core.model.{OrderModel, SortableModel}
+import core.useCase.GenerateSortableUseCase
+import error.useCaseError.GenerateSortableUseCaseError
 import org.scalajs.dom
 import useCase.SortByBubbleSortUseCase
 import mock.ToBeSortedMock
+import mock.inputMock.GenerateSortableInputMock
 
 object Main:
 
     def main(args: Array[String]): Unit =
+        val sortable = GenerateSortableUseCase(
+            GenerateSortableInputMock.success
+        ).left.map(_ match
+            case GenerateSortableUseCaseError.InputFailure(value) => value
+        )
         SortByBubbleSortUseCase(
             SortByBubbleSortInput(
-                SortableModel.from(ToBeSortedMock.ascendingOrder.unsorted),
+                sortable,
                 OrderModel.Ascending
             )
         ) match
@@ -25,6 +34,6 @@ object Main:
                 renderOnDomContentLoaded(
                     dom.document.body,
                     div(
-                        res.map(x => div(x.toString))
+                        res.map(x => div(x.list.map(_.toString).reduce((x, y) => x + " - " + y), whiteSpace := "nowrap"))
                     )
                 )
