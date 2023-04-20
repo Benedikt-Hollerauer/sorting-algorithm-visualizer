2529611b9c2d1311fe4db2e39d352d5050fa6660
Benedikt Hollerauer <hollibene@gmail.com>
2023-04-20 13:10:41 +0200

    implemented SortedModel

--- core/core/model/SortedModel.scala
+++ core/core/model/SortedModel.scala
@@ -2,7 +2,7 @@ package core.model
 
 import error.modelError.SortedModelError
 
-case class SortedModel(
+case class SortedModel private(
 	sortable: SortableModel,
 	changedIndices: List[Int]
 )
@@ -12,5 +12,13 @@ object SortedModel:
 	def from(
 		sortable: SortableModel,
 		mayBeChangedIndices: List[Int]
-	): Either[SortedModelError, SortedModel] = ???
+	): Either[SortedModelError, SortedModel] =
+		if(mayBeChangedIndices.length <= 0) Left(SortedModelError.ToFewChangedIndices(mayBeChangedIndices))
+		else if(mayBeChangedIndices.filter(_ < 0).isEmpty) Left(SortedModelError.NegativeChangedIndices(mayBeChangedIndices))
+		else Right(
+			SortedModel(
+				sortable = sortable,
+				changedIndices = mayBeChangedIndices
+			)
+		)
 
