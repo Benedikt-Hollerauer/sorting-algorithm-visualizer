407e7d8286eeba05b8f306a92555ba536b2f6c9e
Benedikt Hollerauer <hollibene@gmail.com>
2023-05-14 20:52:28 +0200

    added styling to navigation bar

--- presentation/NavigationBar.scala
+++ presentation/NavigationBar.scala
@@ -1,59 +1,65 @@
 import com.raquo.laminar.api.L.{*, given}
 import com.raquo.laminar.nodes.ReactiveHtmlElement
-import org.scalajs.dom.{HTMLDivElement, HTMLImageElement, HTMLUListElement}
+import org.scalajs.dom.{HTMLDivElement, HTMLImageElement, HTMLLIElement, HTMLUListElement}
 
 object NavigationBar:
 
-	import NavigationBarStyle.*
-
 	def getHtml(logoSrc: String, sortingAlgorithms: List[SortingAlgorithm]): ReactiveHtmlElement[HTMLDivElement] =
 		div(
-			cls := menuBar,
+			NavigationBarStyle.navigationBarStyle,
 			getLogo(logoSrc),
 			getNavigationItems(sortingAlgorithms)
 		)
 
 	private def getLogo(logoSrc: String): ReactiveHtmlElement[HTMLImageElement] =
 		img(
-			cls := menuLogoImg,
+			NavigationBarStyle.logoStyle,
 			src := logoSrc
 		)
 
 	private def getNavigationItems(sortingAlgorithms: List[SortingAlgorithm]): ReactiveHtmlElement[HTMLUListElement] =
 		ul(
-			cls := menuItemsUl,
+			NavigationBarStyle.navigationItemsStyle,
 			getSelectSortingAlgorithmMenuItemHtml(sortingAlgorithms)
 		)
 
-	private def getSelectSortingAlgorithmMenuItemHtml(sortingAlgorithms: List[SortingAlgorithm]) =
+	private def getSelectSortingAlgorithmMenuItemHtml(sortingAlgorithms: List[SortingAlgorithm]): List[ReactiveHtmlElement[HTMLLIElement]] =
 		sortingAlgorithms.map: sortingAlgorithm =>
 			li(
-				cls := menuItemLi,
-				idAttr := sortingAlgorithm.toString.toLowerCase,
-				span(
-					cls := menuItemNameSpan,
-					sortingAlgorithm.toString,
-				)
+				NavigationBarStyle.sortingAlgorithmMenuItemStyle,
+				sortingAlgorithm.toString
 			)
 
 object NavigationBarStyle:
 
-	val menuBar = style(
-		width := "100%"
+	val navigationBarStyle = Seq(
+		height := "60px",
+		width := "100%",
+		backgroundColor := "#f5f5f5",
+		display.flex,
+		alignItems.center,
+		padding := "0 20px",
+		boxShadow := "0 2px 5px rgba(0, 0, 0, 0.1)"
 	)
 
-	val menuLogoImg = style(
-
+	val logoStyle = Seq(
+		height := "40px",
+		marginRight := "20px"
 	)
 
-	val menuItemsUl = style(
-
+	val navigationItemsStyle = Seq(
+		listStyle := "none",
+		display.flex,
+		alignItems.center,
+		margin := "0"
 	)
 
-	val menuItemLi = style(
-
-	)
-
-	val menuItemNameSpan = style(
-
+	val sortingAlgorithmMenuItemStyle = Seq(
+		padding := "8px 12px",
+		borderRadius := "4px",
+		backgroundColor := "#ffffff",
+		color := "#333333",
+		fontWeight.bold,
+		marginRight := "10px",
+		cursor.pointer
 	)
