b49761a656c46177aebda4e75e3cdb8545736620
Benedikt Hollerauer <hollibene@gmail.com>
2023-05-29 06:48:52 +0200

    removed unused errors

--- core/error/modelError/SortedModelError.scala
+++
@@ -1,6 +0,0 @@
-package error.modelError
-
-enum SortedModelError {
-	case ToFewChangedIndices(it: List[Int])
-	case NegativeChangedIndices(it: List[Int])
-}
--- core/error/useCaseError/SortByBubbleSortUseCaseError.scala
+++
@@ -1,8 +0,0 @@
-package error.useCaseError
-
-import error.modelError.{NonEmptyListModelError, SortableModelError}
-
-enum SortByBubbleSortUseCaseError:
-
-    case NonEmptyListModelCreationFailed(nonEmptyListModelError: NonEmptyListModelError)
-    case SortableModelCreationFailed(sortableModelError: SortableModelError)
