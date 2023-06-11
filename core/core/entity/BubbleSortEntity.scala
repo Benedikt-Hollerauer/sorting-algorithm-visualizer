ae2958e088a4b874d30c4617fb5ce25707d1bc83
Benedikt Hollerauer <hollibene@gmail.com>
2023-06-11 17:59:23 +0200

    split up code in BubbleSortEnity

--- core/core/entity/BubbleSortEntity.scala
+++ core/core/entity/BubbleSortEntity.scala
@@ -7,69 +7,13 @@ import mock.modelMock.SortableModelMock
 
 object BubbleSortEntity extends SortingAlgorithm:
 
-	def sortAscending(sortable: SortableModel): SortedModel =
-		val res = sortable.valuesWithIndices
-			.list
-			.foldLeft(
-				(LazyList.empty[SortingModel], 0)
-			): (acc, _) =>
-				acc._1.lastOption match
-					case Some(last) => (
-						acc._1 ++ sortable
-							.valuesWithIndices
-							.list
-							.drop(acc._2)
-							.scanLeft(
-								SortingModel.empty
-							): (first, second) =>
-								if (first.focusedIndices._2.value <= second.value) SortingModel(
-									focusedIndices = (
-										first.focusedIndices._2,
-										second
-									),
-									focusedIndicesChanged = false
-								)
-								else SortingModel(
-									focusedIndices = (
-										second,
-										first.focusedIndices._2
-									),
-									focusedIndicesChanged = true
-								),
-						acc._2 + 1
-					)
-					case None => (
-						acc._1 ++ sortable
-							.valuesWithIndices
-							.list
-							.scanLeft(
-								SortingModel.empty
-							): (first, second) =>
-								if (first.focusedIndices._2.value >= second.value) SortingModel(
-									focusedIndices = (
-										first.focusedIndices._2,
-										second
-									),
-									focusedIndicesChanged = false
-								)
-								else SortingModel(
-									focusedIndices = (
-										second,
-										first.focusedIndices._2
-									),
-									focusedIndicesChanged = true
-								),
-						acc._2 + 1
-					)
-			._1.filter:
-				case SortingModel((ValueWithIndexModel(_, IndexModel(index0)), ValueWithIndexModel(_, IndexModel(index1))), _) => index0 != -1 && index1 != -1
-		res.foreach(println)
-		SortedModel(
-			sortableModel = sortable,
-			changes = res
-		)
+	override def sortAscending(sortable: SortableModel): SortedModel =
+		sort(sortable, _ <= _)
 
-	def sortDescending(sortable: SortableModel): SortedModel =
+	override def sortDescending(sortable: SortableModel): SortedModel =
+		sort(sortable, _ >= _)
+
+	private def sort(sortable: SortableModel, comparator: (Int, Int) => Boolean): SortedModel =
 		val res = sortable.valuesWithIndices
 			.list
 			.foldLeft(
@@ -84,7 +28,7 @@ object BubbleSortEntity extends SortingAlgorithm:
 							.scanLeft(
 								SortingModel.empty
 							): (first, second) =>
-								if (first.focusedIndices._2.value <= second.value) SortingModel(
+								if (comparator(first.focusedIndices._2.value, second.value)) SortingModel(
 									focusedIndices = (
 										first.focusedIndices._2,
 										second
@@ -107,7 +51,7 @@ object BubbleSortEntity extends SortingAlgorithm:
 							.scanLeft(
 								SortingModel.empty
 							): (first, second) =>
-								if (first.focusedIndices._2.value >= second.value) SortingModel(
+								if (comparator(first.focusedIndices._2.value, second.value)) SortingModel(
 									focusedIndices = (
 										first.focusedIndices._2,
 										second
@@ -124,7 +68,7 @@ object BubbleSortEntity extends SortingAlgorithm:
 						acc._2 + 1
 					)
 			._1.filter:
-				case SortingModel((ValueWithIndexModel(_, IndexModel(index0)), ValueWithIndexModel(_, IndexModel(index1))), _) => index0 != -1 && index1 != -1
+			case SortingModel((ValueWithIndexModel(_, IndexModel(index0)), ValueWithIndexModel(_, IndexModel(index1))), _) => index0 != -1 && index1 != -1
 		SortedModel(
 			sortableModel = sortable,
 			changes = res
