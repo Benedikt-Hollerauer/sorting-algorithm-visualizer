dd1a110577de8774d8b0712c140b4433854afb6e
Benedikt Hollerauer <hollibene@gmail.com>
2023-06-29 11:34:38 +0200

    added unimplementet swapSortable test

--- core/test/entityTest/VisualizeEntity_Test.scala
+++ core/test/entityTest/VisualizeEntity_Test.scala
@@ -1,7 +1,22 @@
 package test.entityTest
 
+import mock.modelMock.SortableModelMock
+
 object VisualizeEntity_Test:
 
 	object swapSortable_should_return:
 
-		def `SortableModel`: Unit = ???
+		def `SortableModel`: Unit =
+			val firstValueWithIndex = SortableModelMock.unsorted.valuesWithIndices.list(0)
+			val secondValueWithIndex = SortableModelMock.unsorted.valuesWithIndices.list(1)
+			val res = VisualizeEntity.swapSortable(
+				sortable = SortableModelMock.unsorted,
+				swappedValues = (
+					firstValueWithIndex,
+					secondValueWithIndex
+				)
+			)
+			val resList = res.valuesWithIndices.list
+			assert(resList(0) == secondValueWithIndex)
+			assert(resList(1) == firstValueWithIndex)
+			assert(resList.length == SortableModelMock.unsorted.valuesWithIndices.list.length)
