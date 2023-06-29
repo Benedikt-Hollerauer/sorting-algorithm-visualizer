4eef4df0af70fcc34c61c51dd2769157279f3e73
Benedikt Hollerauer <hollibene@gmail.com>
2023-06-29 06:32:05 +0200

    added test for VisualizeSortingUseCase

--- core/test/useCaseTest/VisualizeSortingUseCase_Test.scala
+++ core/test/useCaseTest/VisualizeSortingUseCase_Test.scala
@@ -1,7 +1,26 @@
 package test.useCaseTest
 
+import core.model.SortableModel
+import mock.modelMock.SortableModelMock
+import test.TestUtil.*
+
 object VisualizeSortingUseCase_Test:
 
 	object apply_should_return:
 
-		def `LazyList[SortableModel]`: Unit = ???
+		def `LazyList[SortableModel]`: Unit =
+			val res = Right(
+				VisualizeSortingUseCase(
+					input = VisualizeSortingInputMock.input
+				)
+			)
+			assertRight(res)(
+				(res: LazyList[SortableModel]) =>
+					val headList = res.head.valuesWithIndices.list
+					val lastList = res.last.valuesWithIndices.list
+					Seq(
+						res.head == SortableModelMock.unsorted,
+						res.last == SortableModelMock.sorted,
+						headList.length == lastList.length
+					)
+			)
