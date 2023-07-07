d554f6a3f326cfa2c29318724d40c84e634e04c3
Benedikt Hollerauer <hollibene@gmail.com>
2023-07-07 06:46:26 +0200

    refactored sort method in BubbleSortEntity and added debugging information

--- core/core/entity/BubbleSortEntity.scala
+++ core/core/entity/BubbleSortEntity.scala
@@ -24,41 +24,64 @@ object BubbleSortEntity extends SortingAlgorithm:
 		acc: LazyList[SortingModel] = LazyList.empty[SortingModel],
 		firstIteration: Boolean = true
 	): SortedModel =
-		val test = valuesWithIndices.foldLeft((
-			LazyList.empty[SortingModel],
-			valuesWithIndices
-		)): (acc, _) =>
-			val sortedOnce = sortOnce(acc._2, comparator).getOrElse(acc._1.toList)
-			(
-				acc._1 ++ sortedOnce,
-				sortedOnce.map(x => x.focusedIndices._1) :+ sortedOnce.last.focusedIndices._2 //TODO refactor this to be cleaner
-			)
-		SortedModel(
-			sortableModel = sortable,
-			changes = test._1
-		)
-		//valuesWithIndices match
-		//	case Nil => SortedModel(
-		//		sortable,
-		//		acc
+		//val test = valuesWithIndices.foldLeft((
+		//	LazyList.empty[SortingModel],
+		//	valuesWithIndices
+		//)): (acc, _) =>
+		//	val sortedOnce = sortOnce(acc._2, comparator).getOrElse(acc._1.toList)
+		//	(
+		//		acc._1 ++ sortedOnce,
+		//		sortedOnce.map(x => x.focusedIndices._1) :+ sortedOnce.last.focusedIndices._2 //TODO refactor this to be cleaner
 		//	)
-		//	case valuesWithIndices =>
-		//		val newValuesWithIndices =
-		//			if(firstIteration) valuesWithIndices
-		//			else comparator match
-		//				case OrderModel.Ascending => valuesWithIndices.filterNot(_ == valuesWithIndices.max)
-		//				case OrderModel.Descending => valuesWithIndices.filterNot(_ == valuesWithIndices.min)
-		//		val sortedOnce = sortOnce(newValuesWithIndices, comparator)
-		//		val newAcc = sortedOnce match //TODO this has to go into new valuesWithIndices
-		//			case Some(it) => acc ++ it
-		//			case None => acc
-		//		sort(
-		//			sortedOnce,
-		//			sortable,
-		//			comparator,
-		//			newAcc,
-		//			false
-		//		)
+		//SortedModel(
+		//	sortableModel = sortable,
+		//	changes = test._1
+		//)
+		valuesWithIndices match
+			case Nil => SortedModel(
+				sortable,
+				acc
+			)
+			case valuesWithIndices =>
+				val sortedOnce = sortOnce(valuesWithIndices, comparator)
+				val newAcc = sortedOnce match
+					case Some(it) => acc ++ it
+					case None => acc
+				val newValuesWithIndices = sortedOnce match
+					case Some(it) =>
+						println(it)
+						val newIt = it.map(_.focusedIndices._1) :+ it.last.focusedIndices._2
+						comparator match
+							case OrderModel.Ascending => newIt.filterNot(_ == valuesWithIndices.max)
+							case OrderModel.Descending => newIt.filterNot(_ == valuesWithIndices.min)
+					case None => Nil
+				sort(
+					newValuesWithIndices,
+					sortable,
+					comparator,
+					newAcc,
+					false
+				)
+			//case valuesWithIndices =>
+			//	val newValuesWithIndices =
+			//		if(firstIteration) valuesWithIndices
+			//		else comparator match
+			//			case OrderModel.Ascending => valuesWithIndices.filterNot(_ == valuesWithIndices.max)
+			//			case OrderModel.Descending => valuesWithIndices.filterNot(_ == valuesWithIndices.min)
+			//	val sortedOnce = sortOnce(newValuesWithIndices, comparator)
+			//	val newAcc = sortedOnce match //TODO this has to go into new valuesWithIndices
+			//		case Some(it) => acc ++ it
+			//		case None => acc
+			//	sort(
+			//		sortedOnce match
+			//			case Some(it) => it.map(_.focusedIndices._1) :+ it.last.focusedIndices._2
+			//			case None => Nil
+			//		,
+			//		sortable,
+			//		comparator,
+			//		newAcc,
+			//		false
+			//	)
 
 	def sortOnce(
 		toBeCompared: List[ValueWithIndexModel],
