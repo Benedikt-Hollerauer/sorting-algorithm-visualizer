5a530319800e9fa48c2e2e500a493389a121e2de
Benedikt Hollerauer <hollibene@gmail.com>
2023-07-19 09:26:25 +0200

    added InsertionSortEntity_Test

--- testImpl/Entity_TestImpl.scala
+++ testImpl/Entity_TestImpl.scala
@@ -1,6 +1,7 @@
-import test.entityTest.{BubbleSortEntity_Test, VisualizeEntity_Test}
+import test.entityTest.{BubbleSortEntity_Test, InsertionSortEntity_Test, VisualizeEntity_Test}
 
 class Entity_TestImpl extends TestUtil:
 
 	implementTest(BubbleSortEntity_Test)
 	implementTest(VisualizeEntity_Test)
+	implementTest(InsertionSortEntity_Test)
