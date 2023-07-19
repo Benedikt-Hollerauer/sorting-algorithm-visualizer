8987138525adb6cbb0d025db65667f95b1a3075c
Benedikt Hollerauer <hollibene@gmail.com>
2023-07-19 09:18:42 +0200

    added SortByInsertionSortUseCase_Test

--- testImpl/UseCase_TestImpl.scala
+++ testImpl/UseCase_TestImpl.scala
@@ -1,4 +1,4 @@
-import test.useCaseTest.{GenerateSortableUseCase_Test, VisualizeSortingUseCase_Test}
+import test.useCaseTest.{GenerateSortableUseCase_Test, SortByInsertionSortUseCase_Test, VisualizeSortingUseCase_Test}
 import useCaseTest.SortByBubbleSortUseCase_Test
 
 class UseCase_TestImpl extends TestUtil:
@@ -6,3 +6,4 @@ class UseCase_TestImpl extends TestUtil:
     implementTest(SortByBubbleSortUseCase_Test)
     implementTest(GenerateSortableUseCase_Test)
     implementTest(VisualizeSortingUseCase_Test)
+    implementTest(SortByInsertionSortUseCase_Test)
