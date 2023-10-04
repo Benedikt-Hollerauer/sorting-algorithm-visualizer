0c6777f048f208958b13a55c25539c838ed74538
Benedikt Hollerauer <hollibene@gmail.com>
2023-10-04 07:58:52 +0200

    added using clauses to test cases

--- core/test/entityTest/VisualizeEntity_BubbleSort_Test.scala
+++ core/test/entityTest/VisualizeEntity_BubbleSort_Test.scala
@@ -33,28 +33,36 @@ object VisualizeEntity_BubbleSort_Test:
 
 		private val valuesWithIndices = SortedModelMock.sortedModelBubbleSort.toBeSorted.list
 
-		def `BarModel - BarStateModel.Normal`: Unit =
+		def `BarModel - BarStateModel.Normal`(
+			using getBarModel: GetBarModel[SortingModel]
+		): Unit =
 			val res = VisualizeEntity.getBarModel(
 				valuesWithIndices.head,
 				SortedModelMock.changesBubbleSort(1)
 			)
 			assert(res.barState == BarStateModel.Normal)
 
-		def `BarModel - BarStateModel.Focused`: Unit =
+		def `BarModel - BarStateModel.Focused`(
+			using getBarModel: GetBarModel[SortingModel]
+		): Unit =
 			val res = VisualizeEntity.getBarModel(
 				valuesWithIndices(0),
 				SortedModelMock.changesBubbleSort(13)
 			)
 			assert(res.barState == BarStateModel.Focused)
 
-		def `BarModel - BarStateModel.Swapped`: Unit =
+		def `BarModel - BarStateModel.Swapped`(
+		  	using getBarModel: GetBarModel[SortingModel]
+		): Unit =
 			val res = VisualizeEntity.getBarModel(
 				valuesWithIndices(2),
 				SortedModelMock.changesBubbleSort(1)
 			)
 			assert(res.barState == BarStateModel.Swapped)
 
-		def `BarModel - BarStateModel.AlreadySorted`: Unit =
+		def `BarModel - BarStateModel.AlreadySorted`(
+			using getBarModel: GetBarModel[SortingModel]
+		): Unit =
 			val res = VisualizeEntity.getBarModel(
 				valuesWithIndices.last,
 				SortedModelMock.changesBubbleSort.last
